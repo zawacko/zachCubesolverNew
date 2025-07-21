45331cf4025d126665dc5a0f5060daa11025bf09
amahl <amahlagrawal@gmail.com>
2025-07-20 17:48:19 -0700

    black

--- cubesolver/src/main/java/org/cubexell/cubesolver/core/OpenCvRaspberryPiCamera.java
+++ cubesolver/src/main/java/org/cubexell/cubesolver/core/OpenCvRaspberryPiCamera.java
@@ -252,7 +252,7 @@ public class OpenCvRaspberryPiCamera implements CubeColorInspector{
         referenceColors.put('O', new double[]{50, 128, 128});
 
         referenceColors.put('r', new double[]{60,   167, 155});
-        //referenceColors.put('o', new double[]{100,  170, 170});
+        referenceColors.put('o', new double[]{100,  128, 128});
         referenceColors.put('y', new double[]{140, 120, 180});
         referenceColors.put('g', new double[]{85, 100, 155});
         referenceColors.put('w', new double[]{111, 130, 140});
